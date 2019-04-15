d957ae611b8c45218f002682334437a4f1190b6e
Joao Pedro Pio <joao.pio@agilecontent.com>
2019-04-15 15:46:27 -0300

    Remove unecessary dependencies

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,55 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-export default class NavMenu extends Component {
-  constructor(props) {
-    super(props);
+const NavMenu = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">Content Manager</a>
+        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true,
-    };
-  }
-
-  toggleNavbar() {
-    const { collapsed } = this.state;
-
-    this.setState({
-      collapsed: !collapsed,
-    });
-  }
-
-  render() {
-    const { collapsed } = this.state;
-
-    return (
-      <header>
-        <nav className="navbar navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <div className="container">
-            <a className="navbar-brand" href="/">Content Manager</a>
-            <button type="button" onClick={this.toggleNavbar} className="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div id="navbarSupportedContent" className="collapse navbar-collapse d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <div className="nav-item">
-                  <Link to="/movie">Movie</Link>
-                </div>
-                <div className="nav-item">
-                  <Link to="/mediatest">Media Test</Link>
-                </div>
-                <div className="nav-item">
-                  <Link to="/person">Person</Link>
-                </div>
-                <div className="nav-item">
-                  <Link to="/media">Media</Link>
-                </div>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-    );
-  }
+        <div id="navbarSupportedContent" className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link to="/movie">Movie</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/mediatest">Media Test</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/person">Person</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/media">Media</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
+
+export default NavMenu;
